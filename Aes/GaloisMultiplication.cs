4f0ffff48297fdc8ac383c4e6887d03ce1da06ba
Ardy Foolen <ardyfoolen@abv.bg>
2020-08-07 15:41:41 +0300

    Document GMul functions

--- Aes/GaloisMultiplication.cs
+++ Aes/GaloisMultiplication.cs
@@ -6,7 +6,7 @@ namespace Aes.AF
 {
     public static class GaloisMultiplication
     {
-        #region Galois Field (256) Multiplication of two Bytes
+        #region Galois Field 2(8) Finit Field Multiplication with reducer 0x11B
 
         // TestVectors
         // Before       After
@@ -18,7 +18,8 @@ namespace Aes.AF
         // 2d 26 31 4c  4d 7e bd f8
 
         /// <summary>
-        /// Galois Field (256) Multiplication of two Bytes
+        /// Galois Field 2(8) Finit Field Multiplication with reducer 0x11B
+        /// Method is used in the MixColumns of Aes Encryption and Decryption
         /// Do not use branches, to avoid side channels
         /// </summary>
         /// <param name="a"></param>
@@ -39,20 +40,13 @@ namespace Aes.AF
 
         #endregion
 
-        #region Galois Field (256) Multiplication of two Bytes
-
-        // TestVectors
-        // Before       After
-        // db 13 53 45  8e 4d a1 bc
-        // f2 0a 22 5c  9f dc 58 9d
-        // 01 01 01 01  01 01 01 01
-        // c6 c6 c6 c6  c6 c6 c6 c6
-        // d4 d4 d4 d5  d5 d5 d7 d6
-        // 2d 26 31 4c  4d 7e bd f8
+        #region Galois Field 2(128) Multiplication of two Byte arrays with reducer (x128 + x7 + x2 + x + 1)
 
         /// <summary>
-        /// Galois Field (256) Multiplication of two Bytes
+        /// Galois Field 2(128) Multiplication of two Byte arrays with reducer (x128 + x7 + x2 + x + 1)
+        /// Method is used in Galois Counter Mode (GCM) Authenticated Encryption with Authenticated Data (AEAD)
         /// Do not use branches, to avoid side channels
+        /// Note: unlike the GMul method the multiplication is a shift right and overflow is on the least significant bit 
         /// </summary>
         /// <param name="a"></param>
         /// <param name="b"></param>
