9f79bed088ff8d6c380aa9b98848f2e812a36d90
Ardy Foolen <ardyfoolen@abv.bg>
2022-08-24 10:42:36 +0300

    Added interface method

--- Aes/Factories/IAesFactory.cs
+++ Aes/Factories/IAesFactory.cs
@@ -7,5 +7,6 @@ namespace Aes.AF.Factories
     public interface IAesFactory
     {
         IEncryptorFactory CreateFactory(EncryptModeEnum mode, byte[] aad = null);
+        IEncryptorFactory CreateFactory(AesSettings settings, byte[] aad = null);
     }
 }
