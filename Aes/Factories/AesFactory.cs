19db3cd796fcf11639355473c52b3da3b0ce94ed
Ardy Foolen <ardyfoolen@abv.bg>
2022-08-24 13:15:51 +0300

    Refactor AesFactory

--- Aes/Factories/AesFactory.cs
+++ Aes/Factories/AesFactory.cs
@@ -9,21 +9,9 @@ namespace Aes.AF.Factories
     {
         public IEncryptorFactory CreateFactory(EncryptModeEnum mode, byte[] aad = null)
         {
-            var settings = AesSettings.GetEnumerator();
-
-            switch(mode)
-            {
-                case EncryptModeEnum.ECB:
-                    return new EcbEncryptorFactory(settings.FirstOrDefault(s => EncryptModeEnum.ECB.Equals(s.Mode)));
-                case EncryptModeEnum.CBC:
-                    return new CbcEncryptorFactory(settings.FirstOrDefault(s => EncryptModeEnum.CBC.Equals(s.Mode)));
-                case EncryptModeEnum.CTR:
-                    return new CtrEncryptorFactory(settings.FirstOrDefault(s => EncryptModeEnum.CTR.Equals(s.Mode)));
-                case EncryptModeEnum.GCM:
-                    return new GcmEncryptorFactory(settings.FirstOrDefault(s => EncryptModeEnum.GCM.Equals(s.Mode)), aad);
-                default:
-                    throw new ArgumentOutOfRangeException($"Invalid Mode {mode}");
-            }
+            var settings = AesSettings.GetEnumerator()
+                .FirstOrDefault(s => s.Mode.Equals(mode));
+            return CreateFactory(settings, aad);
         }
 
         public IEncryptorFactory CreateFactory(AesSettings settings, byte[] aad = null)
